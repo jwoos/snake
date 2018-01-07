96ee91343c42ccfe4c32651fe60944831a5eb876
Jun Woo Shin <shin.junwoo97@gmail.com>
2018-01-07 21:51:33 +0000

    vector

---
+++ common.h
@@ -0,0 +1,14 @@
+#ifndef SNAKE_COMMON_H
+#define SNAKE_COMMON_H
+
+
+enum Resize {
+	ADD,
+	MULTIPLY,
+	SUBTRACT,
+	DIVIDE,
+	SET
+};
+
+
+#endif
---
+++ vector.h
@@ -0,0 +1,42 @@
+#ifndef SNAKE_VECTOR_H
+#define SNAKE_VECTOR_H
+
+
+#include <stdint.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+#include "common.h"
+
+
+typedef struct Vector {
+	uint64_t size;
+	uint64_t capacity;
+
+	void** array;
+} Vector;
+
+
+Vector* vectorConstruct(uint64_t);
+
+void vectorDeconstruct(Vector* vector, void (*fn)(void*));
+
+void vectorPush(Vector*, void*);
+
+void* vectorPop(Vector*);
+
+void* vectorGet(const Vector*, uint64_t);
+
+void vectorSet(Vector*, uint64_t, void*);
+
+void vectorInsert(Vector*, uint64_t, void*);
+
+void vectorDelete(Vector*, uint64_t, void (*fn)(void*));
+
+void vectorClear(Vector*, void (*fn)(void*));
+
+void vectorResize(Vector*, enum Resize, uint64_t);
+
+
+#endif
